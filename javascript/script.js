8843d540c3d12b432d1d9e92c6aef0297f8756bf
zygisG13 <zygimantas.guobuzas@gmail.com>
2022-11-25 13:39:55 +0200

    Fixed logic bug's

--- javascript/script.js
+++ javascript/script.js
@@ -3,17 +3,19 @@
 function getComputerSelection(max) {
     let choice = Math.floor(Math.random() * max);
     if (choice === 0) {
+        console.log("rock's")
         return "rock"
     } else if (choice === 1) {
+        console.log("scissors's")
         return "scissors"
     }
     else {
+        console.log("paper's")
         return "paper"
     }
 }
 
 function game(userSelection, computerSelection) {
-    console.log(computerSelection)
     if (userSelection === "rock" && computerSelection === "scissors" || 
         userSelection === "paper" && computerSelection === "rock" ||
         userSelection === "scissors" && computerSelection === "paper") {
@@ -42,23 +44,23 @@ let computerWins = 0;
 
 for (let i = 0; i < 5; i++) {
     let userSelection = prompt("What's your selection?");
+    console.log(`${userSelection.toLowerCase()} as`);
     let max = 3;
     let computerChoise = getComputerSelection(max);
-    console.log(`${userSelection.toLowerCase()} as`);
     if (promptCheck(userSelection.toLowerCase()) === true) {
         if (game(userSelection.toLowerCase(), computerChoise) === true) {
             userWins = ++userWins;
-            console.log(`Result is: User - ${userSelection.toLowerCase()} Computer - ${getComputerSelection(max)}.
+            console.log(`Result is: User - ${userSelection.toLowerCase()} Computer - ${computerChoise}.
             User win!`);
         }
         else if (game(userSelection.toLowerCase(), computerChoise) === "parity") {
-            console.log(`Result is: User - ${userSelection.toLowerCase()} Computer - ${getComputerSelection(max)}.
+            console.log(`Result is: User - ${userSelection.toLowerCase()} Computer - ${computerChoise}.
             Parity!`);
         }
 
         else {
             computerWins = ++computerWins;
-            console.log(`Result is: User - ${userSelection.toLowerCase()} Computer - ${getComputerSelection(max)}.
+            console.log(`Result is: User - ${userSelection.toLowerCase()} Computer - ${computerChoise}.
             Computer win!`);
         }
     }
