16f58b525835514c38af53160d1cb7b31f280739
zygisG13 <zygimantas.guobuzas@gmail.com>
2022-11-25 10:55:21 +0200

    Added wins counter and display who is the winner

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,4 @@
 
-
-
-
 //this function will return random number 0,1 or 2, 0 === rock, 1 === scissors, 2 === paper
 function getComputerSelection(max) {
     return Math.floor(Math.random() * max); 
@@ -15,21 +12,28 @@ function game(userSelection, computerSelection) {
             return true
         }
     else {
-            return false
-        
+            return false       
     }
 }
 
+let userWins = 0;
+let computerWins = 0;
 
 for (let i = 0; i < 5; i++) {
     let userSelection = prompt("What's your selection?");
     let max = 3;
     console.log(userSelection.toLowerCase())
     if (game(userSelection.toLowerCase(), getComputerSelection(max)) === true) {
-        console.log("You Win! Paper beats Rock");
+        userWins = ++userWins;
+        console.log(userWins)
     }
     else
-        console.log("You Lose! Paper beats Rock");
+        computerWins = ++computerWins;
+        console.log(computerWins)
+}
 
-
- }
+if (userWins > computerWins) {
+    console.log(`You win! Score ${userWins} > ${computerWins}`)    
+} else {
+    console.log(`You lost! Score ${computerWins} > ${userWins}`) 
+}
