f4dfc468ff426eb9ba42b3b1e2062e5f165b1c6c
zygisG13 <zygimantas.guobuzas@gmail.com>
2022-12-06 15:12:11 +0200

    index.html added buttons, script.js created playround function, witch is called then user pushed the button

--- javascript/script.js
+++ javascript/script.js
@@ -1,82 +1,73 @@
-
 //this function will return random number 0,1 or 2, 0 === rock, 1 === scissors, 2 === paper
+//with "max" limit random function to 0,1,2 options
 function getComputerSelection(max) {
-    let choice = Math.floor(Math.random() * max);
-    if (choice === 0) {
-        console.log("rock's")
-        return "rock"
-    } else if (choice === 1) {
-        console.log("scissors's")
-        return "scissors"
-    }
-    else {
-        console.log("paper's")
-        return "paper"
-    }
+  let choice = Math.floor(Math.random() * max);
+  if (choice === 0) {
+    //console.log("rock's");
+    return "rock";
+  } else if (choice === 1) {
+    //console.log("scissors's");
+    return "scissors";
+  } else {
+    //console.log("paper's");
+    return "paper";
+  }
 }
 
-//game engine: eveluate choises, return true- user wins, parity, false- computer wins
+//game engine: evaluate choices, return true- user wins, parity, false- computer wins
 function game(userSelection, computerSelection) {
-    if (userSelection === "rock" && computerSelection === "scissors" || 
-        userSelection === "paper" && computerSelection === "rock" ||
-        userSelection === "scissors" && computerSelection === "paper") {
-            return true
-        }
-    else if (userSelection === "rock" && computerSelection === "rock" || 
-            userSelection === "paper" && computerSelection === "paper" ||
-            userSelection === "scissors" && computerSelection === "scissors") {
-                return "parity"
-    } else {
-            return false       
-    }
+  if (
+    (userSelection === "rock" && computerSelection === "scissors") ||
+    (userSelection === "paper" && computerSelection === "rock") ||
+    (userSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return true;
+  } else if (
+    (userSelection === "rock" && computerSelection === "rock") ||
+    (userSelection === "paper" && computerSelection === "paper") ||
+    (userSelection === "scissors" && computerSelection === "scissors")
+  ) {
+    return "parity";
+  } else {
+    return false;
+  }
 }
 
-//checks if user inputs is correct
-function promptCheck(userSelection) {
-    if (userSelection === "rock" || userSelection === "paper" || userSelection 
-    === "scissors") {
-        return true
-    } else {
-        return false
-    }
+//game result
+function playRound(userSelection, computerSelection) {
+  if (game(userSelection, computerSelection) === true) {
+    userWins = ++userWins;
+    console.log(`Result is: User - ${userSelection} 
+            Computer - ${computerSelection}. User win!`);
+  } else if (game(userSelection, computerSelection) === "parity") {
+    console.log(`Result is: User - ${userSelection} 
+            Computer - ${computerSelection}. Parity!`);
+  } else {
+    computerWins = ++computerWins;
+    console.log(`Result is: User - ${userSelection} 
+            Computer - ${computerSelection}. Computer win!`);
+  }
 }
 
-//base loop, calls helping functions, count results
+
 let userWins = 0;
 let computerWins = 0;
 
-for (let i = 0; i < 5; i++) {
-    let userSelection = prompt("What's your selection?");
-    console.log(`${userSelection.toLowerCase()} as`);
-    //with "max" limit random functiuon to 0,1,2 optiopns
-    let max = 3;
-    let computerChoise = getComputerSelection(max);
-    if (promptCheck(userSelection.toLowerCase()) === true) {
-        if (game(userSelection.toLowerCase(), computerChoise) === true) {
-            userWins = ++userWins;
-            console.log(`Result is: User - ${userSelection.toLowerCase()} 
-            Computer - ${computerChoise}. User win!`);
-        }
-        else if (game(userSelection.toLowerCase(), computerChoise) === "parity") {
-            console.log(`Result is: User - ${userSelection.toLowerCase()} 
-            Computer - ${computerChoise}. Parity!`);
-        }
 
-        else {
-            computerWins = ++computerWins;
-            console.log(`Result is: User - ${userSelection.toLowerCase()} 
-            Computer - ${computerChoise}. Computer win!`);
-        }
-    }
-    else {
-        prompt("Wrong Selection");
-           i = --i;
-    }
-}
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    playRound (button.id, getComputerSelection(3))
+  });
+});
 
-//cheking who is the winner after five turns
+
+/*
+//checking who is the winner after five turns
 if (userWins > computerWins) {
-    console.log(`You win! Your score ${userWins} - computer score ${computerWins}`)    
+  console.log(`You win! Your score ${userWins} - computer score ${computerWins}`);
 } else {
-    console.log(`You lost! Computer score ${computerWins} - your score ${userWins}`) 
+  console.log(`You lost! Computer score ${computerWins} - your score ${userWins}`);
 }
+*/
